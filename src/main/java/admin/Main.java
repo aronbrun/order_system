d13eed4a9511cfbbbc8d8971374f05d93ac1f8ce
Tom <tom001.wilhelm@gmail.com>
2019-04-09 10:44:03 +0200

    wokring on adminTool

--- src/main/java/admin/Main.java
+++ src/main/java/admin/Main.java
@@ -1,4 +1,22 @@
 package admin;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Admin Tool");
+        primaryStage.setScene(new Scene(root, 240, 408));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
