0e0623246148a4c777bf57686538268dc0e0b82f
Aron Brun <aronbrun@gmail.com>
2019-05-06 13:54:31 +0200

    design step shop

--- src/main/java/shop/Controller.java
+++ src/main/java/shop/Controller.java
@@ -1,4 +1,58 @@
 package shop;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    ListView<String> iconList = new ListView<String>();
+
+    private final Image IMAGE_RUBY  = new Image("https://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo_64x64.png");
+    private final Image IMAGE_APPLE  = new Image("http://findicons.com/files/icons/832/social_and_web/64/apple.png");
+    private final Image IMAGE_VISTA  = new Image("http://antaki.ca/bloom/img/windows_64x64.png");
+    private final Image IMAGE_TWITTER = new Image("http://files.softicons.com/download/social-media-icons/fresh-social-media-icons-by-creative-nerds/png/64x64/twitter-bird.png");
+
+    private Image[] listOfImages = {IMAGE_RUBY, IMAGE_APPLE, IMAGE_VISTA, IMAGE_TWITTER};
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "RUBY", "APPLE", "VISTA", "TWITTER");
+        iconList.setItems(items);
+
+        iconList.setCellFactory(param -> new ListCell<String>() {
+            private ImageView imageView = new ImageView();
+            @Override
+            public void updateItem(String name, boolean empty) {
+                super.updateItem(name, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    if(name.equals("RUBY"))
+                        imageView.setImage(listOfImages[0]);
+                    else if(name.equals("APPLE"))
+                        imageView.setImage(listOfImages[1]);
+                    else if(name.equals("VISTA"))
+                        imageView.setImage(listOfImages[2]);
+                    else if(name.equals("TWITTER"))
+                        imageView.setImage(listOfImages[3]);
+                    setText(name);
+                    setGraphic(imageView);
+                }
+            }
+        });
+
+
+
+    }
 }
--- src/main/java/shop/Main.java
+++ src/main/java/shop/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("shop.fxml"));
+        primaryStage.setTitle("Shop");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
